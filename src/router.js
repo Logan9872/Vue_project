3fe99de1a779a7676cbab9bfabf19f05dd6d4934
Logan <luchangda_njfu@163.com>
2021-03-31 18:59:17 +0800

    路由分裝

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import  vue from 'vue'
+import Router from 'vue-router' 
+import Home from '.pages/home'
+import Index from '.pages/index'
+impsort Detail from '.pages/deatil'
+pages
+exports default new Router({
+    routes:
+    {
+        path:''
+    }
+})
