fea1835f049c946c8660fa2ddcd4adf589818c34
Logan <luchangda_njfu@163.com>
2021-04-01 14:42:43 +0800

    提交路由更改

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
+import VueAxios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router.js'
+import router from './router'
+
+axios.interceptor.response.use(function(response{
+    let res= response.data
+    if(res,status ==0){
+        return res.data;
+    }else if(res.status ==10){
+        window,location.href ='/#login';
+    }else{
+        alert(res.msg);
+    }
+
+}));
 
 createApp(App).mount('#app')
