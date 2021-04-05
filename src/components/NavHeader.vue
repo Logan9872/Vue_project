6ebe2b316aac20799038f2f2598e852c4b785c6d
Logan <luchangda_njfu@163.com>
2021-04-06 00:40:33 +0800

    导航栏动画

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -25,25 +25,14 @@
         <div class="header-menu">
           <div class="item-menu">
             <span>小米手机</span>
-            <div class="children"></div>
-          </div>
-          <div class="item-menu">
-            <span>RedMi红米</span>
             <div class="children">
               <ul>
                 <li class="product">
                   <a href="" target="_blank">
                     <div class="pro-img">
-                      <img src="" />
-                    </div>
-                    <div class="pro-name">小米CC9</div>
-                    <div class="pro-price">1799元</div>
-                  </a>
-                </li>
-                <li class="product">
-                  <a href="" target="_blank">
-                    <div class="pro-img">
-                      <img src="" />
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/d07671f25a2b3a6c3d4fac189f28fbe9.png?thumb=1&w=160&h=110"
+                      />
                     </div>
                     <div class="pro-name">小米CC9</div>
                     <div class="pro-price">1799元</div>
@@ -51,6 +40,10 @@
                 </li>
               </ul>
             </div>
+          </div>
+          <div class="item-menu">
+            <span>RedMi红米</span>
+            <div class="children"></div>
           </div>
           <div class="item-menu">
             <span>电视</span>
@@ -70,6 +63,30 @@
 <script>
 export default {
   name: "nav-header",
+  data() {
+    return {
+      username: "jack",
+      phoneList: [],
+    };
+  },
+  mounted() {
+    this.getProductList();
+  },
+  methods: {
+    getProductList() {
+      this.axios
+        .get("/products", {
+          params: {
+            categoryId: "100012",
+          },
+        })
+        .then((res) => {
+          if (res.list > 6) {
+            this.phoneList = res.list.slice(0, 6);
+          }
+        });
+    },
+  },
 };
 </script>
 
@@ -109,6 +126,7 @@ export default {
   }
   .nav-header {
     .container {
+      position: relative;
       height: 112px;
       @include flex();
       .header-logo {
@@ -151,28 +169,87 @@ export default {
           }
           &:hover {
             color: $colorA;
+            .children {
+              height: 220px;
+              opacity: 1;
+            }
+          }
+          .children {
+            height: 0;
+            opacity: 0;
+            position: absolute;
+            top: 112px;
+            overflow: hidden;
+            left: 0px;
+            width: 1226px;
+            border-top: 1px solid #e5e5e5;
+            box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
+            z-index: 10;
+            transition: all 0.5s;
+
+            .product {
+              position: relative;
+              float: left;
+              width: 16.6%;
+              height: 20px;
+              font-size: 12px;
+              line-height: 12px;
+              text-align: center;
+              a {
+                display: inline-block;
+              }
+              img {
+                height: 111px;
+                width: auto;
+                margin-top: 26px;
+              }
+              .pro-img {
+                height: 137px;
+              }
+              .pro-name {
+                font-weight: bold;
+                margin-top: 19px;
+                margin-bottom: 8px;
+                color: $colorB;
+              }
+              .pro-price {
+                color: $colorA;
+              }
+              &:before {
+                content: "";
+                position: absolute;
+                top: 28px;
+                right: 0;
+                border-left: 1px solid #d7d7d7;
+                height: 100px;
+                width: 1px;
+              }
+              &:last-child:before {
+                display: none;
+              }
+            }
           }
         }
       }
-      .header-search {
-        width: 319px;
-        .wrapper {
+    }
+    .header-search {
+      width: 319px;
+      .wrapper {
+        height: 50px;
+        border: 1px solid #e0e0e0;
+        display: flex;
+        align-items: center;
+        input {
+          border: none;
+          box-sizing: border-box;
+          border-right: 1px solid #e0e0e0;
+          width: 264px;
           height: 50px;
-          border: 1px solid #e0e0e0;
-          display: flex;
-          align-items: center;
-          input {
-            border: none;
-            box-sizing: border-box;
-            border-right: 1px solid #e0e0e0;
-            width: 264px;
-            height: 50px;
-            padding-left: 14px;
-          }
-          a {
-            @include bgImg(18px, 18px, "/imgs/icon-search.png");
-            margin-left: 17px;
-          }
+          padding-left: 14px;
+        }
+        a {
+          @include bgImg(18px, 18px, "/imgs/icon-search.png");
+          margin-left: 17px;
         }
       }
     }
