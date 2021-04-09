26ac07ad21fa8e8db31abe05d06722ed40bfb449
Logan <luchangda_njfu@163.com>
2021-04-09 21:29:19 +0800

    登录页更改

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import router from './router.js'
 import App from './App.vue'
 import VueLazyLoad from 'vue-lazyload'
 import VueCookie from 'vue-cookie'
-import env from './env'
+// import env from './env'
 
 //根据前端的跨域方式做调整/a/b
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
 
 // 根据环境变量获取不同的请求地址
-axios.defaults.baseURL = env.baseURL;
+// axios.defaults.baseURL = env.baseURL;
 // 接口错误拦截
 axios.interceptors.response.use(function (response) {
     let res = response.data;
